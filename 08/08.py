6c0e09244c56cac0bee52f10fb431babb67f2d54
Elias Wachmann <elias.wachmann@gmail.com>
2024-12-03 15:38:54 +0100

    setup other days

---
+++ 08/08.py
@@ -0,0 +1,11 @@
+
+def read_data(chdir=True):
+    if chdir:
+        import os
+        os.chdir(os.path.dirname(__file__))
+    with open('input.txt') as f:
+        lines = f.readlines()
+    return lines
+
+if __name__ == "__main__":
+    lines = read_data()
