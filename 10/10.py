50ba28613f28d778434f811d59594c3efc1c1517
Elias Wachmann <elias.wachmann@gmail.com>
2024-12-10 11:43:35 +0100

    something from nothing

--- 10/10.py
+++ 10/10.py
@@ -1,4 +1,4 @@
-
+import numpy as np
 def read_data(chdir=True):
     if chdir:
         import os
@@ -7,5 +7,11 @@ def read_data(chdir=True):
         lines = f.readlines()
     return lines
 
+def solvea(lines): 
+    a = np.array([[int(y) for y in x.strip()] for x in lines])
+    trailhead_pos = np.where(a == 0)
+    
+
 if __name__ == "__main__":
     lines = read_data()
+    solvea(lines)
